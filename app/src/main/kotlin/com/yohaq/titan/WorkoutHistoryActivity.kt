06a890f932a5eb61e0828822bf4fd20de21dc131
Yousuf Haque <yousuf.s.haque@gmail.com>
2016-06-06 21:41:24 -0400

    Create Project

---
+++ app/src/main/kotlin/com/yohaq/titan/WorkoutHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.yohaq.titan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_history)
+    }
+}
