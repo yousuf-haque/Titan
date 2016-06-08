3d46d10d0a9402bf69b58d3da7fd5af77955031f
Yousuf Haque <yousuf.s.haque@gmail.com>
2016-06-08 17:33:24 -0400

    Clean up Exercises adapter

--- app/src/main/kotlin/com/yohaq/titan/ui/adapters/ExercisesAdapter.kt
+++ app/src/main/kotlin/com/yohaq/titan/ui/adapters/ExercisesAdapter.kt
@@ -3,13 +3,11 @@ package com.yohaq.titan.ui.adapters
 import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.yohaq.titan.R
 import com.yohaq.titan.data.models.Exercise
 import com.yohaq.titan.databinding.ExerciseItemBinding
-import com.yohaq.titan.presenters.ExerciseCatalogPresenter
 import com.yohaq.titan.ui.viewModels.ExerciseViewModel
 import java.util.*
 import javax.inject.Inject
@@ -19,7 +17,7 @@ import javax.inject.Inject
  */
 class ExercisesAdapter
 @Inject
-constructor(private val presenter: ExerciseCatalogPresenter)
+constructor()
 : RecyclerView.Adapter<ExercisesAdapter.ExercisesViewHolder>() {
 
 
@@ -46,15 +44,16 @@ constructor(private val presenter: ExerciseCatalogPresenter)
         @Suppress("MISSING_DEPENDENCY_CLASS")
         holder.exerciseItemBinding.viewModel = ExerciseViewModel(exercises[position])
 
+        holder.exerciseItemBinding.root.setOnClickListener {
+            view ->
+            Toast.makeText(view.context, exercises[position].name, Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExercisesViewHolder? {
         val exerciseItemBinding: ExerciseItemBinding =
                 DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.exercise_item, parent, false)
-        exerciseItemBinding.root.setOnClickListener ({
-            view: View ->
-            Toast.makeText(view.context, "click", Toast.LENGTH_LONG).show()
-        })
         return ExercisesViewHolder(exerciseItemBinding)
     }
 
