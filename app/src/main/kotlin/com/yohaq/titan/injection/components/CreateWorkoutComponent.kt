0186da4d83910467eea17751bd0a0174928908d4
Yousuf Haque <yousuf.s.haque@gmail.com>
2016-06-09 00:57:38 -0400

    Remove Activity scope in favor of View Scope

--- app/src/main/kotlin/com/yohaq/titan/injection/components/CreateWorkoutComponent.kt
+++ app/src/main/kotlin/com/yohaq/titan/injection/components/CreateWorkoutComponent.kt
@@ -1,12 +1,14 @@
 package com.yohaq.titan.injection.components
 
 import com.yohaq.titan.injection.modules.RealmModule
+import com.yohaq.titan.injection.scopes.ViewScope
 import com.yohaq.titan.ui.activities.CreateWorkoutActivity
 import dagger.Component
 
 /**
  * Created by yousufhaque on 6/8/16.
  */
+@ViewScope
 @Component(
         modules = arrayOf(RealmModule::class)
 )
--- app/src/main/kotlin/com/yohaq/titan/injection/components/ExerciseCatalogComponent.kt
+++ app/src/main/kotlin/com/yohaq/titan/injection/components/ExerciseCatalogComponent.kt
@@ -1,14 +1,14 @@
 package com.yohaq.titan.injection.components
 
 import com.yohaq.titan.injection.modules.RealmModule
-import com.yohaq.titan.injection.scopes.ActivityScope
+import com.yohaq.titan.injection.scopes.ViewScope
 import com.yohaq.titan.ui.views.ExerciseCatalog
 import dagger.Component
 
 /**
  * Created by yousufhaque on 6/8/16.
  */
-@ActivityScope
+@ViewScope
 @Component (
         modules = arrayOf(RealmModule::class)
 )
